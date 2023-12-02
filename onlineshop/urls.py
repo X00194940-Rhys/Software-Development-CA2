6dd041b32655e96fe4f2d4113088cb2134e0a40b
Ethan Ford <x00197969@mytudublin.ie>
2023-12-02 22:53:18 +0000

    vouchers

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('',include('shop.urls')),
     path('search/', include('search_app.urls')),
     path('cart/', include('cart.urls')),
+    path('vouchers/', include('vouchers.urls')),
     path('order/', include('order.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
